598d7bae4d8b70f6f8f1bda56124d62c05e9b759
Nicolas Uthurriague <nicolasu@microsoft.com>
2019-10-31 20:41:35 +0100

    Initial push

---
+++ OpenXMLSDKator/OpenXMLSDKator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace OpenXMLSDKator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Open a WordprocessingDocument for editing using the filepath.
+            WordprocessingDocument wordprocessingDocument =
+                WordprocessingDocument.Open(args[0], true);
+
+            // Assign a reference to the existing document body.
+            Body body = wordprocessingDocument.MainDocumentPart.Document.Body;
+
+            // Close the handle explicitly.
+            wordprocessingDocument.Close();
+        }
+    }
+}
